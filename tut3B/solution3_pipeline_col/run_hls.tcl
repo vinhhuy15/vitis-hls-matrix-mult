5b1d1a540751c1ef3fd1ffe409cd295c3c085cb8
Giang Vĩnh Huy <huy.giang.vinh@gmail.com>
2026-06-03 06:58:24 +0700

    Add Tutorial 3B solution 3

---
+++ tut3B/solution3_pipeline_col/run_hls.tcl
@@ -0,0 +1,16 @@
+open_project -reset matrix_mult_prj
+
+add_files matrix_mult.cpp
+add_files matrix_mult.h
+add_files -tb matrix_mult_test.cpp
+set_top matrix_mult
+
+open_solution -reset solution3
+set_part {xc7z020clg484-1}
+create_clock -period 5
+
+csim_design
+csynth_design
+cosim_design
+
+exit

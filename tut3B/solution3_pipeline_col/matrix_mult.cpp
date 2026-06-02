3b04cd386e9dde2813e03273535619ec385e68c4
Giang Vĩnh Huy <huy.giang.vinh@gmail.com>
2026-06-03 06:57:54 +0700

    Add Tutorial 3B solution 3

---
+++ tut3B/solution3_pipeline_col/matrix_mult.cpp
@@ -0,0 +1,21 @@
+#include "matrix_mult.h"
+
+void matrix_mult(
+    mat_a a[IN_A_ROWS][IN_A_COLS],
+    mat_b b[IN_B_ROWS][IN_B_COLS],
+    mat_prod prod[IN_A_ROWS][IN_B_COLS])
+{
+Row:
+    for (int i = 0; i < IN_A_ROWS; i++) {
+    Col:
+        for (int j = 0; j < IN_B_COLS; j++) {
+#pragma HLS PIPELINE II=1
+            prod[i][j] = 0;
+
+        Product:
+            for (int k = 0; k < IN_B_ROWS; k++) {
+                prod[i][j] += a[i][k] * b[k][j];
+            }
+        }
+    }
+}
